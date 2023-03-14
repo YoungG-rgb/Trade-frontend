018be162dac0512802ab06a77f4e25ceb6187ec1
Zhalaldin Toichubaev <zhtoichubaev@nurtelecom.kg>
2023-03-14 19:01:05 +0600

    перенес в новый репо

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Header = () => {
+
+    const headerMenu = ['Главная','Функции','Наша миссия','О нас','Часы']
+
+    return (
+        <header className='flex items-center justify-between main_container'>
+            <div className='w-[270px]'>
+                <img className='w-full bg-cover' src="/logo/logo.svg" alt="logo"/>
+            </div>
+            <ul className='flex items-center gap-[40px]'>
+                {
+                    headerMenu.map((items)=>(
+                        <li className='not-italic font-normal text-[14px] leading-[12px] text-[#939393] hover:text-[#000000] cursor-pointer
+                  border-2
+                  border-b-white
+                  rounded-b-[2px]
+                  border-t-0
+                  border-r-0
+                  border-l-0
+                  transition
+                  hover:border-b-indigo-500 pb-[5px]'>{items}</li>
+                    ))
+                }
+                <li className='not-italic font-normal text-[14px] leading-[12px] text-[#FFFFFF] hover:bg-blue-700 transition cursor-pointer bg-[#735CFF] py-[22px] px-[54px] rounded-[5px]'>Войти</li>
+            </ul>
+        </header>
+    );
+};
+
+export default Header;
---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const NotFound = () => {
+    return (
+        <div className='flex flex-col items-center pt-[50px]'>
+            <h1 className='not-italic font-normal text-[42px] leading-[50px] text-center text-[#000000] mb-[24px]'>Oops, Something <br/> Went Wrong</h1>
+            <img className='w-[450px]' src="/notFound/notFound.svg" alt="404"/>
+            <button className='mt-[52px] not-italic font-normal leading-[12px] text-[14px] text-white py-[22px] px-[31px] bg-[#735CFF] rounded-[5px]' type='button'>Return to Homepage</button>
+        </div>
+    )
+}
+
+export default NotFound;
