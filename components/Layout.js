3659f707dfba2c2ce6cd336de4c9f71912ae5377
Zhalaldin Toichubaev <zhtoichubaev@nurtelecom.kg>
2023-04-28 14:56:06 +0600

    delete alll admnins components -> replace to back

--- components/Layout.js
+++ components/Layout.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import Header from "@/components/Header";
 import FooterDown from "@/components/FooterDown";
-import dashboard from "@/pages/admin-panel/dashboard";
-import about from "@/pages/admin-panel/aboutus";
-import products from "@/pages/products";
-import slides from "@/pages/admin-panel/slides";
-import users from "@/pages/admin-panel/users";
 
 const Layout = ({children}) => {
-    switch (children.className) {
-        case dashboard.className:
-        case about.className:
-        case products.className:
-        case slides.className:
-        case users.className:
-            return (
-                <>
-                    {children}
-                </>
-            )
-        default:
-            return (
-                <div className=''>
-                    <Header/>
-                    {children}
-                    <FooterDown/>
-                </div>
-            );
-    }
+    return (
+        <div className=''>
+            <Header/>
+            {children}
+            <FooterDown/>
+        </div>
+    );
 };
 
 export default Layout;
